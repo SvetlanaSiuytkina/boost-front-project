b5bc2f06f0e6dfc9fe3c8e3963c039a3edacac24
SVETA <svetadan1234@mail.ru>
2026-09-20 16:16:25 +0500

    corrected

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/') || '';
-
-const base = repoName ? `/\${repoName}/` : '/';
-
 export default defineConfig({
   plugins: [react()],
-  base: base,
+  base: '/boost-front-project/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
